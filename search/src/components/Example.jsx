7da7b102c1aae6442b227777e31f6ab71a0e8455
shibamoto <silverbirder@gmail.com>
2020-08-22 16:24:07 +0900

    😎 Client Side Rendering

--- search/src/components/Example.jsx
+++ search/src/components/Example.jsx
@@ -1,34 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-// import ProductItem from '@bit/silver-birder.sample.product-item';
 
-const Example = ({title}) => <div>
-  <div className="form-group">
-    <label htmlFor="exampleInputIcon2">Search Keyword</label>
-    <div className="input-group mb-4">
-      <input className="form-control" placeholder={title.name} type="text"/>
-      <div className="input-group-append">
-        <button className="input-group-text">
-          <span className="fas fa-search"></span>
-        </button>
-      </div>
-    </div>
-  </div>
-  <div className="row">
-    {['🐙', '🐳', '🐊', '🐍', '🐷', '🐶', '🐯'].map((emoji) => {
-      return <div>
-        <div className="card bg-primary border-light shadow-soft">
-          <div className="card-body">
-            <h3 className="h5 card-title">{emoji}{emoji}{emoji}</h3>
-            <button className="btn btn-icon-only btn-primary" type="button" aria-label="love button" title="love button">
-              <span aria-hidden="true" className="fas fa-shopping-cart"></span>
-            </button>
+class Example extends Component {
+  constructor(props){
+    super(props);
+    this.title = props.title;
+  }
+  alertMessage() {
+    alert('hey');
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="form-group">
+          <label htmlFor="exampleInputIcon2">Search Keyword</label>
+          <div className="input-group mb-4">
+            <input className="form-control" placeholder={this.title.name} type="text"/>
+            <div className="input-group-append">
+              <button className="input-group-text" onClick={this.alertMessage}>
+                <span className="fas fa-search"></span>
+              </button>
+            </div>
           </div>
         </div>
+        <div className="row">
+          {['🐙', '🐳', '🐊', '🐍', '🐷', '🐶', '🐯'].map((emoji, key) => {
+            return <div key={key}>
+              <div className="card bg-primary border-light shadow-soft">
+                <div className="card-body">
+                  <h3 className="h5 card-title">{emoji}{emoji}{emoji}</h3>
+                  <button className="btn btn-icon-only btn-primary" type="button" aria-label="button" title="button">
+                    <span aria-hidden="true" className="fas fa-shopping-cart"></span>
+                  </button>
+                </div>
+              </div>
+            </div>
+          })}
+        </div>
       </div>
-    })}
-  </div>
-</div>;
+    );
+  }
+}
 
 Example.propTypes = {
   title: PropTypes.object.isRequired
